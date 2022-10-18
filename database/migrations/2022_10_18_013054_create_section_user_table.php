1e4981ab9a0cbe13ddd60a97404322b7546426bc
Eugene Kushch <jesthete78@gmail.com>
2022-10-18 04:58:10 +0300

    add factories and seeder

--- database/migrations/2022_10_18_013054_create_section_user_table.php
+++ database/migrations/2022_10_18_013054_create_section_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSectionUsersTable extends Migration
+class CreateSectionUserTable extends Migration
 {
     /**
      * Run the migrations.
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,12 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Section;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      *
@@ -13,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        $sections = Section::factory()->count(10)->create();
+        User::factory()
+        ->hasAttached($sections->random(4))
+        ->create(['email' => 'admin@admin.com']);
+
+        User::factory()
+        ->count(5)
+        ->hasAttached($sections->random(3))
+        ->create();
     }
 }
