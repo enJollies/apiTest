05bb4a3f033a777d6c4a4952941d9881c5f0903e
Eugene Kushch <jesthete78@gmail.com>
2022-10-18 05:51:47 +0300

    add CRUD for User

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\Api\User\StoreRequest;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Api\User\UpdateRequest;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = isset($data['password']) ? Hash::make($data['password']) : Hash::make('password');
+        User::create($data);
+
+        return redirect()->route('users.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRequest $request, User $user)
+    {
+        $data = $request->validated();
+        $user->update($data);
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->sections()->detach();
+        $user->delete();
+        return true;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'Api'], function() {
+
+    Route::group(['prefix' => 'users'], function() {
+        Route::get('/', 'UserController@index')->name('users.index');
+        Route::post('/', 'UserController@store')->name('users.store');
+        Route::get('/{user}', 'UserController@show')->whereNumber('user')->name('users.show');
+        Route::patch('/{user}', 'UserController@update')->whereNumber('user')->name('users.update');
+        Route::delete('/{user}', 'UserController@destroy')->whereNumber('user')->name('users.destroy');
+    });
 });
